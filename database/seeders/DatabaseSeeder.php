7ee6288808853273dde1ed3f4e662f1da370a67b
simueljester <jestercareer@gmail.com>
2026-02-06 22:15:04 +0800

    update readme

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-            'password' => bcrypt('password'),
+            'password' => bcrypt('@test123'),
         ]);
     }
 }
